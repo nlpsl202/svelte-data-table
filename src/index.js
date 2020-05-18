977d509cd9f65bf0e114fc6ba2b74d9dd14a5e4c
kjj6198 <kjj6198@gmail.com>
2020-05-19 02:50:18 +0900

    export TableHead component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default } from "./Table/Table.svelte";
 export { default as TableCell } from "./Table/TableCell.svelte";
 export { default as TableHeadColumn } from "./Table/TableHeadColumn.svelte";
+export { default as TableHead } from "./Table/TableHead.svelte";
